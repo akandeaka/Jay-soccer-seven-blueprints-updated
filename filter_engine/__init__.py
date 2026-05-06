20b286beb64e7e53dea1ad3115247ac6c4af8839
akandeaka <akande.akinpelu913@gmail.com>
2026-05-06 21:33:54 +0100

    Delete filter_engine directory

--- filter_engine/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Filter Engine for JAY SOCCER BLUEPRINTS - Updated with 8 Blueprints
-"""
-
-from .blueprint_filter import BlueprintFilterEngine
-from .telegram_integration import TelegramIntegrator
-from .config import FilterConfig
-
-__all__ = ['BlueprintFilterEngine', 'TelegramIntegrator', 'FilterConfig']
